f3a9604d09fa6ee6087aba4e8425bcf192317650
FAXES <31368070+FAXES@users.noreply.github.com>
2022-03-01 18:14:33 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports.getFile = (url, dest, ...options) => new Promise((resolve, reject) => {
+    const req = url.trim().startsWith('https') ? require('https') : require('http');
+    req.get(url, options, (res) => {
+        if(res.statusCode !== 200) {
+            res.resume();
+            return reject(console.warn(`Request Failed.\nStatus Code: ${res.statusCode}`));
+        }
+        res.pipe(fs.createWriteStream(path.dirname(require.main.filename)+'/'+dest))
+        .on('error', (err) => reject(console.warn(`Request Failed. Error:\n${err}\n`)))
+        .once('close', () => resolve({filename: dest, dir:path.dirname(require.main.filename)+'/'+dest}));
+    })
+    .on('timeout', (err) => reject(console.warn(`Request Failed. Timeout:\n${err}\n`)))
+    .on('error', (err) => reject(console.warn(`Request Failed. Error:\n${err}\n`)));
+})
