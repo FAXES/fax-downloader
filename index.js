393a35d909a4b9a5451497985464591458ac50ec
Josh <31368070+FAXES@users.noreply.github.com>
2026-02-21 13:54:12 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -104,36 +104,3 @@ module.exports.getBuffer = async (url, options = {}) => {
 		return { status: isAbort ? 408 : 500, error: err };
 	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const fs = require('fs');
-const path = require('path');
-
-module.exports.getFile = (url, dest, options = {}) => new Promise((resolve, reject) => {
-    const req = url.trim().startsWith('https') ? require('https') : require('http');
-    req.get(url, options, (res) => {
-        if(res.statusCode !== 200) {
-            res.resume();
-            resolve({status: res.statusCode, error: res.statusCode})
-        }
-        res.pipe(fs.createWriteStream(path.dirname(require.main.filename)+'/'+dest))
-        .on('error', (err) => resolve({status: res.statusCode, error: err}))
-        .once('close', () => resolve({dest: dest, status: res.statusCode}));
-    })
-    .on('timeout', (err) => resolve({status: 408, error: err}))
-    .on('error', (err) => resolve({status: 400 || 500, error: err}));
-})
